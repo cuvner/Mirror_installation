538f3dda316334e529818fe22acbd6cae8b60d9f
cuvner <chriscourage699@hotmail.com>
2021-04-20 11:00:24 +0100

    added mvp6

---
+++ mvp_draft_6/src/FaceDetection.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCvHaarFinder.h"
+#include "ofxOpenCv.h"
+
+class FaceDetection {
+public:
+    FaceDetection();
+    void setup();
+    void update();
+    void setupInstallation();
+    void installation();
+    void nFacesDetected();//No. of faces detetcted at installation
+    void detection(bool _drawRect);// loop over found faces and draw a square
+    
+    ofImage img;
+    ofVideoGrabber videoGrabber;
+    ofxCvHaarFinder finder;
+    ofRectangle cur;
+    
+    ofxCvColorImage colorImg;
+    ofxCvGrayscaleImage init;
+    ofxCvGrayscaleImage reSize;
+    
+    
+    float millis;
+    bool faceDet;
+    int proximity;
+    int nFacesPresent;
+    int counter;
+};
